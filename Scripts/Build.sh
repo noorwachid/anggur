e0ae77ad7c543dacfad8b2f1b214e737723d069a
Noor Wachid <wachid@fisip.net>
2022-11-29 20:00:50 +0700

    rescript build

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -1,5 +1,26 @@
-BUILD_DIR=Distribution
-BUILD_PROGRAM=Application
+PROJECT_EXECUTABLE=Application
+PROJECT_DIRECTORY=Projects
+DISTRIBUTION_DIRECTORY=Distributions
+DISTRIBUTION_OS=""
 
-[ ! -d $BUILD_DIR ] && mkdir -p $BUILD_DIR
-cd $BUILD_DIR && cmake .. && make && cd "./Projects/$BUILD_PROGRAM" && "./$BUILD_PROGRAM" && cd ../..
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    DISTRIBUTION_OS="Linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    DISTRIBUTION_OS="MacOS"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    DISTRIBUTION_OS="Windows"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    DISTRIBUTION_OS="Windows"
+elif [[ "$OSTYPE" == "win32" ]]; then
+    DISTRIBUTION_OS="Windows"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    DISTRIBUTION_OS="FreeBSD"
+else
+    echo "Unknown OS"
+fi
+
+LOCAL_DISTRIBUTION_DIRECTORY="./$DISTRIBUTION_DIRECTORY/$DISTRIBUTION_OS"
+
+[ ! -d $LOCAL_DISTRIBUTION_DIRECTORY ] && mkdir -p $LOCAL_DISTRIBUTION_DIRECTORY
+
+cd $LOCAL_DISTRIBUTION_DIRECTORY && cmake ../.. && make && cd "./$PROJECT_DIRECTORY/$PROJECT_EXECUTABLE" && "./$PROJECT_EXECUTABLE" && cd ../../..
