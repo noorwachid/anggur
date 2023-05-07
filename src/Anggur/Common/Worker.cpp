95f568ff544cc493c220a8f1b84fcb60874f1f56
Noor Wachid <wachid@fisip.net>
2023-05-07 13:16:54 +0700

    Add audio library

--- src/Anggur/Common/Worker.cpp
+++ src/Anggur/Common/Worker.cpp
@@ -42,7 +42,7 @@ namespace Anggur
         }
     }
 
-    ~Worker::Worker() 
+    Worker::~Worker() 
 	{
         {
             std::lock_guard<std::mutex> lock(mutex);
---
+++ vendor/mackron/drlibs/src/dr_wav.c
@@ -0,0 +1,4 @@
+#define DR_WAV_IMPLEMENTATION
+
+#include "dr_wav.h"
+
